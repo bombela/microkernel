6a5cc27373b489a0ea71128576a155550da41ba1
bombela <bombela@gmail.com>
2010-11-29 01:21:15 +0100

    hack success to run constructors/destructors

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,7 @@ SECTION(".multiboot") ALIGNED(4) multiboot::header_short
 	do { \
 		for (int i = 0; i < 200; i++) \
 		{ \
-			char* vmem = (char*) VideoMem + (2 * 80) * line; \
+			char* vmem = (char*) VideoMem + (2 * 80) * (line); \
 			const char* hello = msg; \
 			while (*hello) \
 			{ \
@@ -39,20 +39,62 @@ struct Toto
 void* __dso_handle = 0;
 
 typedef void (*func_ptr)();
-func_ptr cleanup[10] = { 0 };
+func_ptr cleanup[20] = { 0 };
 
-extern "C" void __cxa_atexit(void (*func)())
+void* cleanup_self[20];
+
+extern "C" int __cxa_atexit(void (*func)(), void* self)
 {
 	msg("__cxa_atexit", 6);
 	
 	typedef void (*func_ptr)();
 	func_ptr* fend = cleanup;
-	while (*fend) ++fend;
+	int i = 0;
+	while (*fend)
+	{
+		++fend;
+		++i;
+	}
 	*fend = func;
 	*++fend = 0;
+	cleanup_self[i] = self;
+	return 0;
 }
 
 Toto toto;
+
+template <typename T, int L>
+struct Titi
+{
+	const char* _name;
+
+	template <typename U>
+	Titi(const char* name, U arg1):
+		_name(name),
+		var(arg1)
+	{
+		msg("                                            constructor style", L);
+		msg(name, L);
+	}
+
+	Titi(const char* name):
+		_name(name)
+	{
+		msg("                                            constructor", L);
+		msg(name, L);
+	}
+
+	~Titi() {
+		msg("                                            destructor", L + 1);
+		msg(_name, L + 1);
+	}
+	T var;
+};
+
+Titi<int, 10> a("int");
+Titi<char, 12> b("char");
+
+Titi< Titi<float, 14>, 16> c("Titi<float>", "float");
 
 extern "C" void kernel_main(int magic, void* multiboot_addr)
 {
@@ -73,13 +115,20 @@ extern "C" void kernel_main(int magic, void* multiboot_addr)
 		extern func_ptr __b_ctors[];
 		extern func_ptr __e_ctors[];
 
-		for (func_ptr* i = __e_ctors - 1; i > __b_ctors; --i)
+		for (func_ptr* i = __e_ctors - 1; i >= __b_ctors; --i)
 			(*i)();
 	}
 
 	{
 		msg("call destructor", 24);
-		for (func_ptr* i = cleanup; *i; ++i)
-			(*i)();
+		func_ptr* i = cleanup;
+		int j = 0;
+		while (*i)
+		{
+			++i;
+			++j;
+		}
+		while (--j, --i >= cleanup)
+			((void (*)(void*))(*i)) (cleanup_self[j]);
 	}
 }
