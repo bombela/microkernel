8214ef6799590a0d6ba0d529fdac619687204929
bombela <bombela@gmail.com>
2010-12-02 18:43:45 +0100

    verified ioports + doc

--- include/kernel/ioports.h
+++ include/kernel/ioports.h
@@ -8,30 +8,40 @@
 #ifndef IOPORTS_H
 #define IOPORTS_H
 
-#include <types.h>
+#include <kernel/types.h>
 
+/*
+ * Extended asm brief summary:
+ *
+ * In asm parameters:
+ * "a" stand for accumalator (%eax)
+ * "N" stand for a direct 8 bits unsigned value (i386 sepcific for ioports)
+ * "d" stand for the d register
+ *
+ * In asm code:
+ * "b" stand for byte size
+ * "w" stand for word size
+ * "d" stand for double word size
+ */
+
+namespace kernel {
 namespace io {
 
 namespace out {
 
 inline void byte(uint16_t port, uint8_t data)
 {
-	/*
-	 * "a" stand for accumalator (%eax)
-	 * "N" stand for 8 bits unsigned value (i386 sepcific for ioports)
-	 * "d" stand for the d register (with N: %dl)
-	 */
 	asm volatile ("outb %b0, %w1" :: "a"(data), "Nd"(port));
 }
 
 inline void word(uint16_t port, uint16_t data)
 {
-	asm volatile ("outw %b0, %w1" :: "a"(data), "Nd"(port));
+	asm volatile ("outw %w0, %w1" :: "a"(data), "Nd"(port));
 }
 
-inline void qword(uint16_t port, uint32_t data)
+inline void dword(uint16_t port, uint32_t data)
 {
-	asm volatile ("outl %b0, %w1" :: "a"(data), "Nd"(port));
+	asm volatile ("outl %d0, %w1" :: "a"(data), "Nd"(port));
 }
 
 } // namespace out
@@ -41,26 +51,27 @@ namespace in {
 inline uint8_t byte(uint16_t port)
 {
 	uint8_t ret;
-	asm volatile ("inb %w1, %0" : "=a"(ret) : "Nd"(port));
+	asm volatile ("inb %w1, %b0" : "=a"(ret) : "Nd"(port));
 	return ret;
 }
 
 inline uint16_t word(uint16_t port)
 {
 	uint16_t ret;
-	asm volatile ("inw %w1, %0" : "=a"(ret) : "Nd"(port));
+	asm volatile ("inw %w1, %w0" : "=a"(ret) : "Nd"(port));
 	return ret;
 }
 
 inline uint32_t dword(uint16_t port)
 {
 	uint32_t ret;
-	asm volatile ("inl %w1, %0" : "=a"(ret) : "Nd"(port));
+	asm volatile ("inl %w1, %d0" : "=a"(ret) : "Nd"(port));
 	return ret;
 }
 
 } // namespace in
 
 } // namespace io
+} // namespace kernel
 
 #endif /* IOPORTS_H */
--- include/kernel/types.h
+++ include/kernel/types.h
@@ -8,6 +8,7 @@
 #ifndef TYPES_H
 #define TYPES_H
 
+typedef unsigned char           uint8_t;
 typedef unsigned short          uint16_t;
 typedef unsigned int            uint32_t;
 typedef unsigned long long      uint64_t;
