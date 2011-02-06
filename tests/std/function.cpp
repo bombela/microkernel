c291df7c6b9ca58acebc6ed6a3f7e63dae8a79ac
bombela <bombela@gmail.com>
2011-02-07 00:01:44 +0100

    init files for function holder support

---
+++ tests/std/function.cpp
@@ -0,0 +1,15 @@
+/*
+ * function.cpp
+ * Copyright © 2011, François-Xavier 'Bombela' Bourlet <bombela@gmail.com>
+ *
+*/
+
+#include <test.h>
+#include <kernel/std/function>
+#include <kernel/std/bind>
+
+namespace kstd = ::std::kernel_std;
+
+BOOST_AUTO_TEST_CASE(fct_basic)
+{
+}
