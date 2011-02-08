c06b6d3e8ff3f3b20bfe2ceb749d4ce50555a2da
bombela <bombela@gmail.com>
2011-02-08 01:55:22 +0100

    two different implementation

--- tests/std/memfn.cpp
+++ tests/std/memfn.cpp
@@ -18,9 +18,11 @@ struct Toto {
 	void modif(int& m) { m += 5; }
 };
 
+// ---- store PMF version
+
 BOOST_AUTO_TEST_CASE(basic_ref)
 {
-	kstd::mem_fn_impl_m<void (Toto::*)()> f(&Toto::job);
+	kstd::mem_fn_storepmf::mem_fn_impl<void (Toto::*)()> f(&Toto::job);
 
 	Toto t;
 	f(t);
@@ -29,7 +31,7 @@ BOOST_AUTO_TEST_CASE(basic_ref)
 
 BOOST_AUTO_TEST_CASE(basic_ptr)
 {
-	kstd::mem_fn_impl_m<void (Toto::*)()> f(&Toto::job);
+	kstd::mem_fn_storepmf::mem_fn_impl<void (Toto::*)()> f(&Toto::job);
 
 	Toto t;
 	f(&t);
@@ -38,7 +40,7 @@ BOOST_AUTO_TEST_CASE(basic_ptr)
 
 BOOST_AUTO_TEST_CASE(basic_args)
 {
-	kstd::mem_fn_impl_m<void (Toto::*)(int)> f(&Toto::hihi);
+	kstd::mem_fn_storepmf::mem_fn_impl<void (Toto::*)(int)> f(&Toto::hihi);
 
 	Toto t;
 	f(t, 42);
@@ -46,7 +48,7 @@ BOOST_AUTO_TEST_CASE(basic_args)
 
 BOOST_AUTO_TEST_CASE(basic_rvalue)
 {
-	kstd::mem_fn_impl_m<void (Toto::*)(int&)> f(&Toto::modif);
+	kstd::mem_fn_storepmf::mem_fn_impl<void (Toto::*)(int&)> f(&Toto::modif);
 
 	Toto t;
 	int a = 10;
@@ -56,7 +58,7 @@ BOOST_AUTO_TEST_CASE(basic_rvalue)
 
 BOOST_AUTO_TEST_CASE(basic_const)
 {
-	kstd::mem_fn_impl_c<void (Toto::*)(int) const> f(&Toto::job2);
+	kstd::mem_fn_storepmf::mem_fn_impl<void (Toto::*)(int) const> f(&Toto::job2);
 
 	Toto t;
     f(const_cast<const Toto&>(t), 2);
@@ -66,18 +68,90 @@ BOOST_AUTO_TEST_CASE(basic_const)
 BOOST_AUTO_TEST_CASE(helper_ref)
 {
 	Toto a;
-	kstd::mem_fn(&Toto::job)(a);
+	kstd::mem_fn_storepmf::mem_fn(&Toto::job)(a);
 	BOOST_CHECK(a._v == 10);
 }
 
 BOOST_AUTO_TEST_CASE(helper_ptr)
 {
 	Toto a;
-	kstd::mem_fn(&Toto::job)(&a);
+	kstd::mem_fn_storepmf::mem_fn(&Toto::job)(&a);
 	BOOST_CHECK(a._v == 10);
 }
 
 BOOST_AUTO_TEST_CASE(helper_const)
+{
+	Toto a;
+	kstd::mem_fn_storepmf::mem_fn(&Toto::job2)(a, 42);
+	BOOST_CHECK(a._v == 12);
+	a._v = 2;
+	kstd::mem_fn_storepmf::mem_fn(&Toto::job2)(const_cast<const Toto*>(&a), 42);
+	BOOST_CHECK(a._v == 12);
+}
+
+// ---- static version
+
+BOOST_AUTO_TEST_CASE(fast_basic_ref)
+{
+	auto f = kstd::mem_fn_builder(&Toto::job).build<&Toto::job>();
+
+	Toto t;
+	f(&t);
+	f(t);
+	BOOST_CHECK(t._v == 10);
+}
+
+BOOST_AUTO_TEST_CASE(fast_basic_ptr)
+{
+	kstd::mem_fn_impl<void (Toto::*)()> f(&Toto::job);
+
+	Toto t;
+	f(&t);
+	BOOST_CHECK(t._v == 10);
+}
+
+BOOST_AUTO_TEST_CASE(fast_basic_args)
+{
+	kstd::mem_fn_impl<void (Toto::*)(int)> f(&Toto::hihi);
+
+	Toto t;
+	f(t, 42);
+}
+
+BOOST_AUTO_TEST_CASE(fast_basic_rvalue)
+{
+	kstd::mem_fn_impl<void (Toto::*)(int&)> f(&Toto::modif);
+
+	Toto t;
+	int a = 10;
+	f(t, a);
+	BOOST_CHECK(a == 15);
+}
+
+BOOST_AUTO_TEST_CASE(fast_basic_const)
+{
+	kstd::mem_fn_impl<void (Toto::*)(int) const> f(&Toto::job2);
+
+	Toto t;
+    f(const_cast<const Toto&>(t), 2);
+	BOOST_CHECK(t._v == 12);
+}
+
+BOOST_AUTO_TEST_CASE(fast_helper_ref)
+{
+	Toto a;
+	kstd::mem_fn(&Toto::job)(a);
+	BOOST_CHECK(a._v == 10);
+}
+
+BOOST_AUTO_TEST_CASE(fast_helper_ptr)
+{
+	Toto a;
+	kstd::mem_fn(&Toto::job)(&a);
+	BOOST_CHECK(a._v == 10);
+}
+
+BOOST_AUTO_TEST_CASE(fast_helper_const)
 {
 	Toto a;
 	kstd::mem_fn(&Toto::job2)(a, 42);
